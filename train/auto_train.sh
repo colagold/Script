8b23af2b3df9835851b264370597625500943024
Yangfighting <1346380661@qq.com>
2022-03-09 16:52:11 +0800

    训练脚本

--- train/auto_train.sh
+++ train/auto_train.sh
@@ -1,8 +1,9 @@
+
 file=out/lr.txt
-lr=(0.01 1e-3 1e-4 1e-5)
-DEVICES=(0 1 2 3)
-for i in ${lr[@]};do
-    CUDA_VISIBLE_DEVICES=0 python run.py -d data/douban -p protocol_head_data.TVT -a algorithm.autodebias.adapter.Autodebias -r "{base_model_emb_dim:8,base_model_imputaion_lambda:0.0001,base_model_learning_rate:$i,weight1_model_learning_rate:$i,weight2_model_learning_rate:$i,imputation_model_learning_rate:$i}"  >> $file &
+lr=(0.01 0.001 0.0001 0.00001)
+DEVICE_NUMS=4
+for i in ${!lr[@]};do
+    CUDA_VISIBLE_DEVICES=$(($i%$DEVICE_NUMS)) python run.py -d data/small_data -p protocol_head_data.TVT -a algorithm.autodebias.adapter.Autodebias -r "{base_model_emb_dim:8,base_model_imputaion_lambda:0.0001,base_model_learning_rate:${lr[$i]},weight1_model_learning_rate:${lr[$i]},weight2_model_learning_rate:${lr[$i]},imputation_model_learning_rate:${lr[$i]}"  >> $file &
 done
 wait
 echo "train end"
---
+++ train/autodebias/douban/lambda4.sh
@@ -0,0 +1,2 @@
+file=out/douban_autodebias/lambda2.txt
+CUDA_VISIBLE_DEVICES=0 python run.py -d data/douban -p protocol_head_data.TVT -a algorithm.autodebias.adapter.Autodebias -r '{base_model_emb_dim:64,base_model_learning_rate:0.001,weight1_model_learning_rate:0.001,weight2_model_learning_rate:0.001,imputation_model_learning_rate:0.001,base_model_imputaion_lambda:0.0001,base_model_weight_decay:0.0001,weight1_model_weight_decay:0.0001,weight2_model_weight_decay:0.0001,imputation_model_weight_decay:0.0001,imputation_model_bias:-0.1,ratio:0.95}' >> $file
